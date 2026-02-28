ae5a182989eebce2c4ecf367933ad06e30b53431
Muxammadali Ergashev <Muxammadali.Ergashev@greenwhite.uz>
2026-02-28 11:37:14 +0500

    fix

--- src/main/java/uz/greenwhite/gateway/oauth2/GrantType.java
+++ src/main/java/uz/greenwhite/gateway/oauth2/GrantType.java
@@ -1,6 +1,19 @@
 package uz.greenwhite.gateway.oauth2;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum GrantType {
-    CLIENT_CREDENTIALS,
-    REFRESH_TOKEN
+    CLIENT_CREDENTIALS("client_credentials"),
+    REFRESH_TOKEN("refresh_token");
+
+    private final String value;
+
+    GrantType(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
 }
